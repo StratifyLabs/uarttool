a776f8d7d82bc6ff21b02381e2572f03357d49ae
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-03-26 12:36:00 -0600

    update for cmake variables

--- src/sl_config.h
+++ src/sl_config.h
@@ -6,10 +6,10 @@
 #ifndef SL_CONFIG_H_
 #define SL_CONFIG_H_
 
-#define SL_CONFIG_VERSION_STRING "0.1"
-#define SL_CONFIG_VERSION_BCD 0x01
+#define SL_CONFIG_VERSION_STRING "0.2"
+#define SL_CONFIG_VERSION_BCD 0x02
 #define SL_CONFIG_DOCUMENT_ID "FyzncMe0fzxh4KVrgdID"
-#define SL_CONFIG_NAME "uartprobe"
+#define SL_CONFIG_NAME "uarttool"
 #define SL_CONFIG_TYPE "app"
 #define SL_CONFIG_PUBLISHER "Stratify Labs, Inc"
 #define SL_CONFIG_PERMISSIONS "public"
